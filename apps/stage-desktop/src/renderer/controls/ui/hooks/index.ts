e8e30a8e87225203b4c5a0347648809d043f6cc3
TTAWDTT <2451989@tongji.edu.cn>
2026-02-03 14:52:03 +0800

    chore: remove unused code/assets

--- apps/stage-desktop/src/renderer/controls/ui/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export { useToast, type ToastItem } from "./useToast";
-export { useSearch, type SearchMatch, type UseSearchOptions, type UseSearchResult } from "./useSearch";
-export { useThemeSettings, type Theme, type UseThemeSettingsResult } from "./useThemeSettings";
-export { useScrollLock, type UseScrollLockResult } from "./useScrollLock";
